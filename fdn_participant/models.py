7bcce3365567a209d40cb4b6a26e27b7083d9450
Erik Butcher <butcer0@gmail.com>
2020-06-07 15:38:12 -0400

    FEAT-5 - Introduce EnvExposure Data Model
    Introduce EnvExposure Data Model

--- fdn_participant/models.py
+++ fdn_participant/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 
 class GeneMutation(models.Model):
-    gene_name = models.CharField(max_length=200)
+    gene_name = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.gene_name
+
+
+class EnvExposure(models.Model):
+    exposure_name = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.exposure_name
