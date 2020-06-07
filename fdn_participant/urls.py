5978a5a68bd5f67f572fa2b646e82fe7f6231cfc
Erik Butcher <butcer0@gmail.com>
2020-06-07 01:21:02 -0400

    FEAT-3 - Stand-Up Participant Information Overview Page
    Stand-Up Participant Information Overview Page

--- fdn_participant/urls.py
+++ fdn_participant/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 app_name = 'fdn_participant'
 urlpatterns = [
-    re_path(r'^$', views.register_participant, name='index'),
+    re_path(r'^$', views.home, name='index'),
+    # ex: /fdn/home
+    path('home', views.home, name='home'),
     # ex: /fdn/registration
     path('registration', views.register_participant, name='registration'),
 ]
