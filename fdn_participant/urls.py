d1effda13c9c8e02c5061816ee2ec9b2be4cc87f
Erik Butcher <butcer0@gmail.com>
2020-06-08 22:58:22 -0400

    FEAT-16 - Implemented Participant Information Overview Page
    Implemented Participant Information Overview Page with Ajax Review Status Update Persistence

--- fdn_participant/urls.py
+++ fdn_participant/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     re_path(r'^$', views.home, name='index'),
     # ex: /fdn/home
     path('home', views.home, name='home'),
+    # ex: /fdn/update_reviewed_status
+    path('update_reviewed_status', views.update_reviewed_status, name='update_reviewed_status'),
     # ex: /fdn/registration
     path('registration', views.register_participant, name='registration'),
     # ex: /fdn/registration/add
